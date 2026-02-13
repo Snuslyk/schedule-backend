cdd5fa96265b9c82223a4c5ae403a28edfd5359c
Snuslyk <127945008+Snuslyk@users.noreply.github.com>
2026-02-13 23:42:36 +0300

    fix: only 7 days in week template

--- src/validators/even-or-odd-week.validator.ts
+++ src/validators/even-or-odd-week.validator.ts
@@ -6,10 +6,10 @@ import {
 @ValidatorConstraint({ name: "EvenOrOdd", async: false })
 export class EvenOrOddWeekConstraint implements ValidatorConstraintInterface {
   validate(days: any[]): Promise<boolean> | boolean {
-    return Array.isArray(days) && (days.length === 7 || days.length === 14)
+    return Array.isArray(days) && (days.length === 7)
   }
 
   defaultMessage(): string {
-    return "There must be exactly 7 or 14 days"
+    return "There must be exactly 7"
   }
 }
